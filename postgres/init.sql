c47752b2925e581d961afb43c1a6e08a2d1df722
sashoor <sarmadashoor1@gmail.com>
2024-09-02 12:03:26 -0700

    .gitignore file addition

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,5 +1,7 @@
 -- Sample SQL initialization script
-CREATE TABLE sample_table (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
