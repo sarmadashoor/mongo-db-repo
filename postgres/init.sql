d76b50a2141d8201c347b3cbe57064d1ebdcfa8a
sashoor <sarmadashoor1@gmail.com>
2024-09-02 12:10:27 -0700

    Stop tracking Django, React, Node, Mongo, and Postgres directories

--- postgres/init.sql
+++
@@ -1,7 +0,0 @@
--- Sample SQL initialization script
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
